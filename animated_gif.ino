1fc80848f9a1c77cbde93128dc799861d9ee2a44
charles <cgiguere@iname.com>
2023-08-06 17:49:51 -0400

    readImageFile

--- animated_gif.ino
+++ animated_gif.ino
@@ -19,17 +19,20 @@
 
 // Uncomment the image to display
 //
-// #define GIF_IMAGE starwars
-// #define GIF_IMAGE trench
-// #define GIF_IMAGE hyperspace
-// #define GIF_IMAGE nostromo
-// #define GIF_IMAGE darthvader
-// #define GIF_IMAGE hud_1
-#define GIF_IMAGE hud_2
-// #define GIF_IMAGE hud_5
-// #define GIF_IMAGE hud_6
-// #define GIF_IMAGE hud_7
-// #define GIF_IMAGE x_wing
+// #define image starwars
+// #define image trench
+// #define image hyperspace
+// #define image nostromo
+// #define image darthvader
+// #define image hud_1
+// #define image hud_2
+// #define image hud_5
+// #define image hud_6
+// #define image hud_7
+// #define image x_wing
+
+uint8_t *gif_image;
+int image_size;
 
 #define chipSelectSDCard 12
 File gif_file;
@@ -47,12 +50,13 @@ void setup()
   tft.fillScreen(TFT_BLACK);
 
   gif.begin(BIG_ENDIAN_PIXELS);
+  gif_image = readImageFile("darthvader.gif");
 }
 
 void loop()
 {
   // Put your main code here, to run repeatedly:
-  if (gif.open((uint8_t *)GIF_IMAGE, sizeof(GIF_IMAGE), GIFDraw))
+  if (gif_image && gif.open((uint8_t *)gif_image, image_size, GIFDraw))
   {
     Serial.printf("Successfully opened GIF; Canvas size = %d x %d\n", gif.getCanvasWidth(), gif.getCanvasHeight());
     tft.startWrite(); // The TFT chip slect is locked low
@@ -74,4 +78,45 @@ void initSDCard(void)
       ;
   }
   Serial.println("SD initialization done.");
+}
+
+uint8_t *readImageFile(String filename)
+{
+  uint8_t *image;
+
+  gif_file = SD.open("/" + filename);
+
+  if (!gif_file)
+  {
+    Serial.println("Failed to open file for reading");
+    return NULL;
+  }
+  // Find out the size of the file to create a suitable buffer
+  image_size = gif_file.size();
+  image = (uint8_t *)malloc(image_size);
+
+  if (image == NULL)
+  {
+    Serial.print("Failed to allocate memory:");
+    Serial.println(image_size);
+    gif_file.close();
+    return NULL;
+  }
+
+  // Read the entire file into the buffer
+  size_t bytesRead = gif_file.read(image, image_size);
+
+  if (bytesRead != image_size)
+  {
+    Serial.println("Failed to read complete file");
+    return NULL;
+  }
+  else
+  {
+    Serial.println("Success to read complete file");
+  }
+
+  // free(image); // Free the allocated memory
+  gif_file.close(); // Close the file
+  return image;
 }
