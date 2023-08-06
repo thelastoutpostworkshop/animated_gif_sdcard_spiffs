f5bc5cb76a58e2ebd8bda18c690f570ebea563fc
charles <cgiguere@iname.com>
2023-08-06 16:43:28 -0400

    initSDCard

--- animated_gif.ino
+++ animated_gif.ino
@@ -1,11 +1,5 @@
 // Animated GIF with Round Display
 //
-// ESP32 40MHz SPI single frame rendering performance
-// Note: no DMA performance gain on smaller images or transparent pixel GIFs
-//
-// #define USE_DMA       // ESP32 ~1.25x single frame rendering performance boost for badgers.h
-                      // Note: Do not use SPI DMA if reading GIF images from SPI SD card on same bus as TFT
-#define NORMAL_SPEED  // Comment out for rame rate for render speed test
 
 #include <SPI.h>
 #include <TFT_eSPI.h>
@@ -43,28 +37,18 @@ File gif_file;
 AnimatedGIF gif;
 TFT_eSPI tft = TFT_eSPI();
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
 
-  if (!SD.begin(chipSelectSDCard))
-  {
-    Serial.println("SD Card initialization failed!");
-    while (1)
-      ;
-  }
-  Serial.println("SD initialization done.");
-
+  initSDCard();
   tft.begin();
-#ifdef USE_DMA
-  tft.initDMA();
-#endif
   tft.setRotation(2);
   tft.fillScreen(TFT_BLACK);
 
   gif.begin(BIG_ENDIAN_PIXELS);
 }
 
-#ifdef NORMAL_SPEED // Render at rate that is GIF controlled
 void loop()
 {
   // Put your main code here, to run repeatedly:
@@ -80,26 +64,14 @@ void loop()
     tft.endWrite(); // Release TFT chip select for other SPI devices
   }
 }
-#else // Test maximum rendering speed
-void loop()
-{
-  long lTime = micros();
-  int iFrames = 0;
 
-  if (gif.open((uint8_t *)GIF_IMAGE, sizeof(GIF_IMAGE), GIFDraw))
+void initSDCard(void)
+{
+  if (!SD.begin(chipSelectSDCard))
   {
-    tft.startWrite(); // For DMA the TFT chip slect is locked low
-    while (gif.playFrame(false, NULL))
-    {
-      // Each loop renders one frame
-      iFrames++;
-      yield();
-    }
-    gif.close();
-    tft.endWrite(); // Release TFT chip select for other SPI devices
-    lTime = micros() - lTime;
-    Serial.print(iFrames / (lTime / 1000000.0));
-    Serial.println(" fps");
+    Serial.println("SD Card initialization failed!");
+    while (1)
+      ;
   }
+  Serial.println("SD initialization done.");
 }
-#endif
